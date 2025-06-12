5010758264cf2eecaae7c4e75d4e48aa2d830c6b
AlifKurnia14 <tsimpsons056@gmail.com>
2025-06-12 15:06:57 +0700

    jika FRONT_position <= REAR_position

--- file1.cpp
+++ file1.cpp
@@ -59,7 +59,37 @@ class Queues {
         }
         else
         {
-            // 
+            // jika elemen yg dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max-1)
+            FRONT =0;
+            else
+            FRONT = FRONT+1;
+
         }
+
+    }
+
+    void display(){
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // cek apakah antrian kosong 
+        if (FRONT == -1){
+            cout << "Queue is empty\n";
+            return;
+
+        }
+
+        cout << "\nElement in the queue are ..\n";
+        // jika FRONT_position <== REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position){
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position]<< " ";
+                FRONT_position++;
+
+            }
+            cout << endl;
+        }
+        else 
     }
 }
