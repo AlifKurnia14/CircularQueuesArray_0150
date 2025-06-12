d90f63d9e6406e8329bc8b3ed56e3f8df07410bf
AlifKurnia14 <tsimpsons056@gmail.com>
2025-06-12 14:48:13 +0700

    mengecek apakah antrian penuh

--- file1.cpp
+++ file1.cpp
@@ -18,5 +18,10 @@ class Queues {
         cout << "Enter a number :";
         cin >> num;
         cout << endl;
+
+        // 1. cek apakah antrian penuh
+        if (( FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1 )){
+            cout << 
+        }
     }
 }
